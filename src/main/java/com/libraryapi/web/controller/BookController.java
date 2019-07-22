02812ba55d3e77e16ae470e231ea96f99550af20
Aini452 <amandine.andre04@gmail.com>
2019-07-22 06:59:56 +0200

    model Book

--- src/main/java/com/libraryapi/web/controller/BookController.java
+++ src/main/java/com/libraryapi/web/controller/BookController.java
@@ -1,7 +1,10 @@
 package com.libraryapi.web.controller;
+import com.libraryapi.model.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
 
 @RestController
 public class BookController {
@@ -12,7 +15,8 @@ public class BookController {
     }
 
     @GetMapping(value="/Books/{id}")
-    public String ListingOneBook(@PathVariable int id){
-        return "Vous avez demandé le livre " + id;
+    public Book ListingOneBook(@PathVariable int id){
+        Book test = new Book(id, "Test", "", "image", new Timestamp( 2019));
+        return test;
     }
 }
